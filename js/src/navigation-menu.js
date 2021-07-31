09d18d2b30736a4c1c1d8bd4fec8ebd30a34ae0c
rendoruiz <rendo.ruiz@outlook.com>
2021-07-30 21:44:24 -0600

    fix closing animation jitter caused by browser scrollbar

--- js/src/navigation-menu.js
+++ js/src/navigation-menu.js
@@ -39,12 +39,40 @@ document.querySelectorAll('[data-nav-id]').forEach((menu) => {
   }
 });
 
+const browserScrollbarWidth = getScrollbarWidth();
 const openNavigation = (body, navMenu) => {
+  // add scrollbar width to make closing animation smoother
+  navMenu.style.setProperty('--scrollbar-width', `${browserScrollbarWidth}px`);
   body.dataset.navStatus = 'active';
   navMenu.dataset.navStatus = 'active';
 }
 
 const closeNavigation = (body, navMenu) => {
+  navMenu.style.setProperty('--scrollbar-width', '0px');
   body.dataset.navStatus = 'closed';
   navMenu.dataset.navStatus = 'closed';
+}
+
+
+// https://stackoverflow.com/a/13382873
+function getScrollbarWidth() {
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+
 }
