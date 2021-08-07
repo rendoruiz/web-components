ea5b3f2fee236242e599ddd869cb8824183e3be0
rendoruiz <rendo.ruiz@outlook.com>
2021-08-07 13:25:16 -0600

    fix translatey if there is a grid gap

--- js/src/image-gallery-swap.js
+++ js/src/image-gallery-swap.js
@@ -8,49 +8,52 @@ galleryItems.forEach((item, itemIndex) => {
     const itemImage = item.querySelector('img');
     const itemImageSource = itemImage.src;
 
+    if (galleryFocusedItem.dataset.status !== 'inprogress') {
+      // set to inprogress to avoid unintended effects
+      galleryFocusedItem.dataset.status = 'inprogress';
 
-    // animate focused item
-    focusedItemImage.style.transition = `width 200ms ease-out, height 100ms ease-out, transform 200ms ease-out`;
-    focusedItemImage.style.width = `${itemImage.clientWidth}px`;
-    focusedItemImage.style.height = `${itemImage.clientHeight}px`;
-    focusedItemImage.style.transform = `translate(calc(100% * ${itemIndex}), 200%) scale(1.03)`;
+      // animate focused item bottom swap
+      focusedItemImage.style.transition = `width 100ms ease-out, height 100ms ease-out, transform 200ms ease-out`;
+      focusedItemImage.style.width = `${itemImage.clientWidth}px`;
+      focusedItemImage.style.height = `${itemImage.clientHeight}px`;
+      focusedItemImage.style.transform = `translate(calc(100% * ${itemIndex}), calc(200% + var(--grid-gap))) scale(1.05)`;
 
-    // animate selected item
-    itemImage.style.position = 'absolute';
-    itemImage.style.transition = 'width 200ms ease-out, height 100ms ease-out, transform 200ms ease-out';
-    itemImage.style.width = `${focusedItemImage.clientWidth}px`;
-    itemImage.style.height = `${focusedItemImage.clientHeight}px`;
-    itemImage.style.transform = `translate(calc(-100% / 3 * ${itemIndex}), -100%) scale(1.03)`;
+      // animate selected item top swap
+      itemImage.style.position = 'absolute';
+      itemImage.style.transition = 'width 100ms ease-out, height 100ms ease-out, transform 200ms ease-out';
+      itemImage.style.width = `${focusedItemImage.clientWidth}px`;
+      itemImage.style.height = `${focusedItemImage.clientHeight}px`;
+      itemImage.style.transform = `translate(calc(-100% / 3 * ${itemIndex}), calc(-100% - var(--grid-gap))) scale(1.05)`;
 
-    // bobbing animation 
-    setTimeout(() => {
-      focusedItemImage.style.transition = 'transform 100ms ease-out';
-      focusedItemImage.style.transform = `translate(calc(100% * ${itemIndex}), 200%) scale(1.0)`;
-
-      itemImage.style.transition = 'transform 100ms ease-out';
-      itemImage.style.transform = `translate(calc(-100% / 3 * ${itemIndex}), -100%) scale(1.0)`;
-
-      // reset properties
+      // post-bobbing animation 
       setTimeout(() => {
-        focusedItemImage.style.transition = 'none';
-        focusedItemImage.style.width = '100%';
-        focusedItemImage.style.height = '100%';
-        focusedItemImage.style.transform = 'unset';
-        
-        itemImage.style.transition = 'none';
-        itemImage.style.width = '100%';
-        itemImage.style.height = '100%';
-        itemImage.style.transform = 'unset';
-        itemImage.style.position = 'static';
-        
-        itemImage.src = focusedItemImage.src;
-        focusedItemImage.src = itemImageSource;
-      }, 100);
-    }, 200);
+        focusedItemImage.style.transition = 'transform 100ms ease-out';
+        focusedItemImage.style.transform = `translate(calc(100% * ${itemIndex}), calc(200% + var(--grid-gap))) scale(1.0)`;
+        itemImage.style.transition = 'transform 100ms ease-out';
+        itemImage.style.transform = `translate(calc(-100% / 3 * ${itemIndex}), calc(-100% - var(--grid-gap))) scale(1.0)`;
 
+        // reset & set
+        setTimeout(() => {
+          // reset property changes & animation
+          focusedItemImage.style.transition = 'none';
+          focusedItemImage.style.width = '100%';
+          focusedItemImage.style.height = '100%';
+          focusedItemImage.style.transform = 'unset';
+          itemImage.style.transition = 'none';
+          itemImage.style.width = '100%';
+          itemImage.style.height = '100%';
+          itemImage.style.transform = 'unset';
+          itemImage.style.position = 'static';
+          
+          // switch images
+          itemImage.src = focusedItemImage.src;
+          focusedItemImage.src = itemImageSource;
 
-
-    // switch images
+          // reset status
+          galleryFocusedItem.dataset.status = null;
+        }, 100);
+      }, 200);
+    }
     
   })
 })
